da88fae8e91ccf93df3026c8a3b12e01820105d9
Adit Parashar <parasharadit86@gmail.com>
2025-02-06 01:08:43 +0530

    updated code

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -5,8 +5,7 @@ import Card from './components/Card'
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      
+      <Card/>
     </div>
   )
 }
--- reacting/src/components/Card.jsx
+++ reacting/src/components/Card.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
 function Card() {
+  const data = [
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMt_kSL3s1phTYoFvQG6tdWiuWJ_2KZqhYEQ&s",
+      name: "Amazon Basics",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit consectetur natus accusamus odit placeat corrupti ratione! Pariatur omnis molestiae explicabo et, a sequi?",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCoZDq40i6vix8yfH7VGmMg112z72s_X6I9Q&s",
+      name: "Apple",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit consectetur natus accusamus odit placeat corrupti ratione! Pariatur omnis molestiae explicabo et, a sequi?",
+    },
+    {
+      image:
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVG-tC00LcPnlXaz1LGE00a_yhPyqqDRIM2Q&s",
+      name: "Daily objects",
+      description:
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit consectetur natus accusamus odit placeat corrupti ratione! Pariatur omnis molestiae explicabo et, a sequi?",
+    },
+
+  ];
+
   return (
-    <div className=''>
-      
+    <div className="w-full h-screen bg-zinc-200 flex justify-center items-center gap-5">
+      {data.map((elem, idx) => (
+        <div
+          key={idx}
+          className="w-52 bg-white rounded-md overflow-hidden shadow-md"
+        >
+          <div className="w-full h-32 bg-zinc-300">
+            <img className="w-full h-full object-cover" src={elem.image} alt={elem.name} />
+          </div>
+          <div className="px-3 py-4">
+            <h2 className="font-semibold">{elem.name}</h2>
+            <p className="text-xs mt-2 text-gray-600">{elem.description}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
