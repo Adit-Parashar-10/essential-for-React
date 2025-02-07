6fb66fa663e8b00fee370886011b771fb581b73a
Adit Parashar <parasharadit86@gmail.com>
2025-02-07 22:35:54 +0530

    updated code

--- reacting/src/components/Card.jsx
+++ reacting/src/components/Card.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 
-function Card() {
-
+function Card({values}) {
+  const {img,name,pro} = values
   const data = [
     
   ]
-  
+
 
   return (
-    <div className='w-full h-screen bg-zinc-300 flex flex-col gap-4 justify-center items-center'>
+    <div className='w-52 bg-white rounded-md overflow-hidden'>
+      <div className='w-full h-32 bg-sky-400'>
+        <img className='w-full h-full object-cover' src={img} alt="" />
+      </div>
+      <div className='w-full p-3'>
+        <h3 className='text-xl font-semibold'>{name}</h3>
+        <h5 className='text-xs'>{pro}</h5>
+        <button onClick={()=>alert("adding as a friend")} className={`px-3 py-1 mt-4 text-white bg-blue-500 font-semibold rounded-md m-3`}>
+        Add friend
+      </button>
+      </div>
       
     </div>
   )
