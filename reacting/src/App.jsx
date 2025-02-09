e0842dfb354b5159d471b189a7846072dc818a6e
Adit Parashar <parasharadit86@gmail.com>
2025-02-09 23:48:06 +0530

    updated code

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     {name:"John", pro:"painter", img:"https://plus.unsplash.com/premium_photo-1672239496290-5061cfee7ebb?q=80&w=387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", friend:false},
     {name:"Amit", pro:"artist", img:"https://plus.unsplash.com/premium_photo-1689562473471-6e736b8afe15?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fHw%3D",friend:false },
     {name:"Rahul", pro:"faltu", img:"https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDEzfHx8ZW58MHx8fHx8",friend:false},
-    {name:"Sumit", pro:"coder", img:"https://plus.unsplash.com/premium_photo-1676391019683-f94cdf1a1078?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI2fHx8ZW58MHx8fHx8",friend:false},
+   // {name:"Sumit", pro:"coder", img:"https://plus.unsplash.com/premium_photo-1676391019683-f94cdf1a1078?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI2fHx8ZW58MHx8fHx8",friend:false},
     
   ]
 
