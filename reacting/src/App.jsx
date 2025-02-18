94be29533058fdf9c423c564bc9d4d9af4694aca
Adit Parashar <parasharadit86@gmail.com>
2025-02-18 23:45:30 +0530

    updated code

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -12,8 +12,7 @@ const App = () => {
   return (
     <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
       <div className='container mx-auto'>
-        <Cards/>
-        <Form/>
+        
       </div>
     </div>
   )
