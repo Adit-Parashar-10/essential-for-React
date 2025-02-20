462f5a7202f8496787e3dc5727e19c7a3af08eb5
Adit Parashar <parasharadit86@gmail.com>
2025-02-21 00:33:26 +0530

    updated code

--- reacting/src/App.jsx
+++ reacting/src/App.jsx
@@ -1,40 +1,44 @@
-import React, { useRef, useState } from 'react'
-import {useForm} from 'react-hook-form'
-import Cards from './components/Cards'
-import Card from './components/Card'
-import Form from './components/Form'
+import React, { useRef, useState } from "react";
+
+import { useForm } from "react-hook-form";
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Nav from "./components/Nav";
+import User from "./components/User";
+import About from "./components/About";
 
 const App = () => {
-
-  const {register, handleSubmit} = useForm();
-
+  const { register, handleSubmit } = useForm();
 
   return (
-    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
-      <Link to="/">home</Link> 
-      <Link to="/user">user</Link> 
-      <Link to="/about">about</Link> 
-    </div>
-  )
-}
+    <>
+      <Nav/>
 
-export default App
+      <Routes>
+        <Route path="/" element ={<Home/>}  />
+        <Route path="/about" element ={<About/>}  />
+        <Route path="/user" element ={<User/>}  />
+      </Routes>
 
+    </>
+  );
+};
 
+export default App;
 
 // <div className='w-full h-screen bg-zinc-300 flex gap-4 items-center justify-center'>
 //      {realdata.map((item,idx) => (
 //       <Card key={idx} handleClick={handleFriendsButton}  values={item}/>
 //      ))}
 
-  // const [realdata , setRealData] = useState(data)
-  // const handleFriendsButton = () => {
-  //   setRealData( (previous)=> {
-  //     return previous.map( (item,index)=>{
-  //       if(index === 2){
-  //         return {...item, friend: !item.friend}
-  //       }
-  //       return item;
-  //     })
-  //   })
-  // }
+// const [realdata , setRealData] = useState(data)
+// const handleFriendsButton = () => {
+//   setRealData( (previous)=> {
+//     return previous.map( (item,index)=>{
+//       if(index === 2){
+//         return {...item, friend: !item.friend}
+//       }
+//       return item;
+//     })
+//   })
+// }
